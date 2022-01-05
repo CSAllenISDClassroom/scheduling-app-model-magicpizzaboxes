b74d070a8992981818296e5167f8df939622d359
Reese Hoffart <reese.hoffart@student.allenisd.org>
2022-01-05 10:27:48 -0600

    added session id system

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,10 +2,32 @@ const express = require('express')
 const mysql = require('mysql2')
 const fs = require('fs')
 const app = express()
-const port = 8888
+const port = 10442
+
+var sessions = []
+
+app.post('/session', (req, res) => {
+  let sessionId = Math.floor(Math.random() * 999999999)
+  sessions.push({'sessionId':sessionId})
+  res.send({'sessionId':sessionId})
+})
+
+// This is v dangerous and is only included for testing purposes!
+app.get('/session', (req, res) => {
+  res.send(sessions)
+})
+
+app.get('/session/:sessionId', (req, res) => {
+  let sessionId = req.params.sessionId
+  let sessionData = sessions.find(element => element.sessionId == sessionId)
+  res.send(sessionData)
+})
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  dbConnection.query("select * from Courses", function (err, result) {
+    if (err) throw err;
+    res.send(result)
+  })
 })
 
 ///// STARTUP INITIALIZATION /////
