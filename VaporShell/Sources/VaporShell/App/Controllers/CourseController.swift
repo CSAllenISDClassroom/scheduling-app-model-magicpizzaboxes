c389da7036856345a3dead7df236cdd20bd6d7c3
Tariq Mahamid <tariq.mahamid@student.allenisd.org>
2022-01-19 00:32:59 -0600

    Added Function to convert Bit Map to Period Numbers

--- VaporShell/Sources/VaporShell/App/Controllers/CourseController.swift
+++ VaporShell/Sources/VaporShell/App/Controllers/CourseController.swift
@@ -16,6 +16,53 @@ public class ClassController{
     ///
     /// Returns ``Employee``
 
+    private func bitToPeriods(bit: Int) -> [Int] {
+        if (bit < 11) {return [bit]}
+
+        switch (bit) {
+        case 11:
+            return [0, 1]
+        case 12:
+            return [1, 2]
+        case 13:
+            return [2, 3]
+        case 14:
+            return [3, 4]
+        case 15:
+            return [4, 5]
+        case 16:
+            return [5, 6]
+        case 17:
+            return [6, 7]
+        case 18:
+            return [7, 8]
+        case 19:
+            return [8, 9]
+        case 20:
+            return [9, 10]
+        case 21:
+            return [2, 5]
+        case 22:
+            return [3, 6]
+        case 23:
+            return [4, 7]
+        default:
+            return [Int]()
+        }
+    }
+    
+    private func getCoursesFromBitMap(bitMap: Int) -> [[Int]] {
+        var currentBitMap = bitMap
+        var periods = [[Int]]()
+
+        while (currentBitMap > 0) {
+            let bit = Int(log2(Double(currentBitMap)))
+            currentBitMap -= Int(pow(Double(2), Double(bit)))
+            periods.append(bitToPeriods(bit: bit))
+        }
+
+        return periods
+    }
     
     public func getClassesById(_ app: Application) throws {
         app.get("courses", ":id") { req -> Class in
