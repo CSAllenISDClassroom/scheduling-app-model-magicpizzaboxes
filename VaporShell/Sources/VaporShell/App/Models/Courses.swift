8f6d502f319436347bbd72dddf9d5ce482bb0262
Sahil Sheth <sahil@sheth.org>
2022-01-20 09:34:55 -0600

    Made slgiht changes to name

--- VaporShell/Sources/VaporShell/App/Models/Courses.swift
+++ VaporShell/Sources/VaporShell/App/Models/Courses.swift
@@ -19,7 +19,7 @@ import FluentMySQLDriver
 
 // UNCOMMENT-DATABASE to configure database example
 // // Content conformance will ensure that the object can be encoded and decoded from HTTP messages.
-final class Class: Model, Content {
+final class Course: Model, Content {
     // Name of the table or collection.
     static let schema = "CourseSectionsView"
 
